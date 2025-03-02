a0562db4b6e7a4dcdf9fa3a48eacc6c451cc0530
lucas2mz <lucasandrada2606@gmail.com>
2025-03-02 20:15:57 -0300

    Tests sin terminar

--- part2/tests/places_test.py
+++ part2/tests/places_test.py
@@ -39,30 +39,71 @@ class TestPlacesEndpoints(unittest.TestCase):
         })
         self.assertEqual(response.status_code, 400)
     
-    def test_list_all_places(self):
+    def test_list_all_places(self): # Test list all places
         response = self.client.get('/api/v1/places')
         self.IsInstance(response.json, list)
         self.assertEqual(response.status_code, 200)
     
     def test_get_place_details(self): # Terminar (falta manejar error 404)
-        place_id = self.test_create_place()
-        response = self.client.get(f'/api/v1/places/{place_id}')
-        self.assertIn(response.status_code, [200, 404])
-        if response.status_code == 200:
-            places = response.json
-            self.assertIsInstance(places, list)
-            if places:
-                for place in places:
-                    self.assertIn("id", place)
-                    self.assertIn("title", place)
-                    self.assertIn("description", place)
-                    self.assertIn("latitude", place)
-                    self.assertIn("longitude", place)
-                    self.assertIn("owner", place)
-                    self.assertIn("amenities", place) # Corregir
+        usuario = self.client.post('/api/v1/users/', json={
+            "first_name": "Jane",
+            "last_name": "Doe",
+            "email": "jane.doe@example.com"
+        })
+
+        owner = usuario.json
+        self.assertIn('id', owner)
+        owner_id = owner['id']
+
+        response = self.client.post('/api/v1/places/', json={
+            "title": "Cozy Apartment",
+            "description": "A nice place to stay",
+            "price": 100.0,
+            "latitude": 37.7749,
+            "longitude": -122.4194,
+            "owner_id": f"{owner_id}"
+        })
+
+        place = response.json
+        self.assertIn('id', place)
+        place_id = place['id']
+
+        details = self.client.get(f'/api/v1/places/{place_id}')
+        self.assertEqual(details.status_code, 200)
+
+        self.assertEqual(place_details['id'], place_id)
+        self.assertEqual(place_details['title'], "Cozy Apartment")
+        self.assertEqual(place_details['description'], "A nice place to stay")
+        self.assertEqual(place_details['price'], 100.0)
+        self.assertEqual(place_details['latitude'], 37.7749)
+        self.assertEqual(place_details['longitude'], -122.4194)
+        self.assertEqual(place_details['owner'], owner)
 
     def test_update_place(self): # Test Success request
-        response = self.client.put(f'/api/v1/places/{place_id}', json={
+        usuario = self.client.post('/api/v1/users/', json={
+            "first_name": "Jane",
+            "last_name": "Doe",
+            "email": "jane.doe@example.com"
+        })
+
+        owner = usuario.json
+        self.assertIn('id', owner)
+        owner_id = owner['id']
+
+        response = self.client.post('/api/v1/places/', json={
+            "title": "Cozy Apartment",
+            "description": "A nice place to stay",
+            "price": 100.0,
+            "latitude": 37.7749,
+            "longitude": -122.4194,
+            "owner_id": f"{owner_id}"
+        })
+
+        place = response.json
+        self.assertIn('id', place)
+        place_id = place['id']
+
+        update = self.client.put(f'/api/v1/places/{place_id}', json={
             "title": "Luxury Condo",
             "description": "An upscale place to stay",
             "price": 200.0
@@ -76,8 +117,7 @@ class TestPlacesEndpoints(unittest.TestCase):
         })
         self.assertEqual(response.status_code, 400)
 
-        invalid_place_id = str(uuid.uuid4())
-        response = self.client.put(f'/api/v1/places/{invalid_place_id}', json={
+        response = self.client.put('/api/v1/places/789342', json={
             "title": "Luxury Condo",
             "description": "An upscale place to stay",
             "price": 200.0
