cf788c49f25fa4ee2317da45e4ead92ef0eaf21f
lucas2mz <lucasandrada2606@gmail.com>
2025-03-11 17:46:16 -0300

    JWT Auth

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -1,16 +1,20 @@
 from flask import Flask
 from flask_restx import Api
+from flask_jwt_extended import JWTManager
+from flask_bcrypt import Bcrypt
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
-from flask_bcrypt import Bcrypt
+from app.api.v1.auth import api as auth_ns
 
 bcrypt = Bcrypt()
+jwt = JWTManager()
 
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
     bcrypt.init_app(app)
+    jwt.init_app(app)
     app.config.from_object(config_class)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
 
@@ -19,4 +23,5 @@ def create_app(config_class="config.DevelopmentConfig"):
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
+    api.add_namespace(auth_ns, path='/api/v1/auth')
     return app
