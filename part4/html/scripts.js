8e3047d05c22bba1429448af017585ed9dd51de8
FedeAngeriz <10086@holbertonstudents.com>
2025-03-30 20:31:31 -0300

    update images

--- part4/html/scripts.js
+++ part4/html/scripts.js
@@ -5,4 +5,44 @@
 
 document.addEventListener('DOMContentLoaded', () => {
     /* DO SOMETHING */
-  });
+    const loginForm = document.getElementById('login-form');
+
+    if (loginForm) {
+        loginForm.addEventListener('submit', async (event) => {
+            event.preventDefault();
+            // Your code to handle form submission
+            const email = document.getElementById('email').value;
+            const password = document.getElementById('password').value;
+
+            try {
+              await loginUser(email, password);
+            } catch (error) {
+              console.error('Error:', error);
+              alert('Error to Login');
+            }
+          });
+        }
+});
+
+async function loginUser(email, password) {
+  try {
+    const response = await fetch('https://your-api-url/login', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ email, password })
+    });
+    // Handle the response
+    if (response.ok) {
+      const data = await response.json();
+      document.cookie = `token=${data.access_token}; path=/`;
+      window.location.href = 'index.html';
+  } else {
+      alert('Login failed: ' + response.statusText);
+  }
+} catch (error) {
+  console.error('Error request:', error)
+  alert('Could not connect to the server')
+}
+}
