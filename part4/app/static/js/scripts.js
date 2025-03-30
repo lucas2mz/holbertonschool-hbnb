190607219af7a823fe211cb7936cd4cf50b0ed25
lucas2mz <lucasandrada2606@gmail.com>
2025-03-30 16:30:51 -0300

    Place scripts

--- part4/app/static/js/scripts.js
+++ part4/app/static/js/scripts.js
@@ -136,3 +136,94 @@ function displayPlaces(places) {
     placesList.appendChild(placeArticle);
   });
 }
+
+function getPlaceIdFromURL() {
+  const params = new URLSearchParams(window.location.search);
+  return params.get('place_id');
+}
+
+function checkAuthentication() {
+  const token = getCookie('token');
+  const addReviewSection = document.getElementById('add-review');
+
+  if (!token) {
+      addReviewSection.style.display = 'none';
+  } else {
+      addReviewSection.style.display = 'block';
+      // Store the token for later use
+      const placeId = getPlaceIdFromURL();
+      fetchPlaceDetails(token, placeId);
+  }
+}
+
+async function fetchPlaceDetails(token, placeId) {
+  // Make a GET request to fetch place details
+  try {
+    const response = await fetch(`http://127.0.0.1:5000/api/v1/places/${placeId}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      }
+    });
+    if (!response.ok) {
+      throw new Error('Failed to fetch place details');
+    }
+
+    const place = await response.json();
+
+    return place;
+  
+  }catch(error) {
+    alert(`Error: ${error.message}`);
+  }
+}
+
+function displayPlaceDetails(place) {
+  const placeDetails = document.getElementById('place-details');
+  placeDetails.innerHTML = '';
+
+  place.array.forEach(place => {
+    const placeDiv = document.createElement('div');
+    placeDiv.classList.add('place-section');
+
+    const placeDiv2 = document.createElement('div');
+    placeDiv2.classList.add('text-content');
+
+    const title = document.createElement('h2');
+    title.classList.add('place-info-title');
+    title.innerHTML = place.title;
+
+    const host = document.createElement('p');
+    host.classList.add('place-info');
+    host.innerHTML = `Host: ${place.host}`;
+
+    const price = document.createElement('p');
+    price.classList.add('place-info');
+    price.innerHTML = `Price per night: ${place.price}`;
+
+    const description = document.createElement('p');
+    description.classList.add('place-info');
+    description.innerHTML = `Description: ${place.description}`;
+
+    const amenities = document.createElement('p');
+    amenities.classList.add('place-info');
+    amenities.innerHTML = `Amenities: ${place.amenities}`;
+
+    if (place.reviews){
+      const reviews = document.createElement('p');
+      reviews.classList.add('place-info');
+      reviews.innerHTML = `Reviews: ${place.reviews}`;
+      placeArticle.appendChild(reviews);
+    }
+
+    placeDiv2.appendChild(title);
+    placeDiv2.appendChild(host);
+    placeDiv2.appendChild(price);
+    placeDiv2.appendChild(description);
+    placeDiv2.appendChild(amenities);
+
+    placeDetails.appendChild(placeDiv);
+    placeDetails.appendChild(placeDiv2);
+  });
+}
